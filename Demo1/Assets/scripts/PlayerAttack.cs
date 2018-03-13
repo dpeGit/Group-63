61fb033e4280af4e28d31b58ea8b2a388629aa3f
DemonPopeEnterprises <mattmrad62@gmail.com>
2018-03-13 09:10:59 -0400

    Melee Attacks and Collisions
    Framework for melee attacks, and colliders now work proper.

---
+++ Demo1/Assets/scripts/PlayerAttack.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private bool isAttacking = false;
+    private float attackTimer = 0f;
+
+    public float attackCD;
+    public float range;
+
+    private void Update()
+    {
+        if (!isAttacking)
+        {
+            if (Input.GetAxis("Fire1") == -1)
+            {
+                isAttacking = true;
+                attackTimer = attackCD;
+
+                Collider2D[] hitObjects = Physics2D.OverlapCircleAll(new Vector2(transform.position.x - range, transform.position.y), range);
+                for(int i = 1; i < hitObjects.Length; i++)
+                {
+                    Debug.Log("Hit " + hitObjects[i].name);
+                }
+            }
+            else if(Input.GetAxis("Fire1") == 1)
+            {
+                isAttacking = true;
+                attackTimer = attackCD;
+
+                Collider2D[] hitObjects = Physics2D.OverlapCircleAll(new Vector2(transform.position.x + range, transform.position.y), range);
+                for (int i = 1; i < hitObjects.Length; i++)
+                {
+                    Debug.Log("Hit " + hitObjects[i].name);
+                }
+            }
+            else if (Input.GetAxis("Fire2") == 1)
+            {
+                isAttacking = true;
+                attackTimer = attackCD;
+
+                Collider2D[] hitObjects = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y + range), range);
+                for (int i = 1; i < hitObjects.Length; i++)
+                {
+                    Debug.Log("Hit " + hitObjects[i].name);
+                }
+            }
+            else if (Input.GetAxis("Fire2") == -1)
+            {
+                isAttacking = true;
+                attackTimer = attackCD;
+
+                Collider2D[] hitObjects = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y - range), range);
+                for (int i = 1; i < hitObjects.Length; i++)
+                {
+                    Debug.Log("Hit " + hitObjects[i].name);
+                }
+            }
+        }
+        if (isAttacking)
+        {
+            if(attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                isAttacking = false;
+            }
+        }
+    }
+    //uncoment this function if you need to check ranges
+    /*
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(new Vector3(transform.position.x - range, transform.position.y, transform.position.z), range);
+        Gizmos.DrawSphere(new Vector3(transform.position.x + range, transform.position.y, transform.position.z), range);
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - range, transform.position.z), range);
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y + range, transform.position.z), range);
+    }*/
+}
---
+++ Demo1/Assets/scripts/movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D player;
+    private Vector2 playerMovement;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        if (moveHoriz == 0 && moveVert == 0)
+        {
+            player.velocity = Vector2.zero;
+        }
+        else
+        {
+            playerMovement = new Vector2(moveHoriz, moveVert);
+            player.velocity = playerMovement * speed;
+        }
+    }
+}
