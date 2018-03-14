0ba514f3249f42dbca97f105150d84f707593638
LAPTOP-A254O95P\mattm <mmr337@drexel.edu>
2018-03-14 16:49:35 -0400

    Merged UI and Game
    finished merging the UI stuff with the gameplay elements use this folder from now on

---
+++ TOAST/Assets/Scripts/enemy/EnemyChase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+
+    private Rigidbody2D enemy;
+
+    private void Start()
+    {
+        enemy = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+         enemy.velocity = (player.transform.position - enemy.transform.position).normalized * speed;
+	}
+}
