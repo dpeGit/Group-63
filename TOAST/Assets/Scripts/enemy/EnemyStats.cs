9970468bbd05abf7c3e01f2ac43f1616077c976e
LAPTOP-A254O95P\mattm <mmr337@drexel.edu>
2018-03-14 18:11:38 -0400

    Enemy Stats and knockback
    Implmented enemy stats framework, and a knock back/ health loss on hit

---
+++ TOAST/Assets/Scripts/enemy/EnemyStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+    public float health;
+    public float speed;
+    public float mass;
+    public float expValue;
+    public float knockbackTimer;
+
+    private Rigidbody2D rgbd;
+    private EnemyChase script;
+
+	// Use this for initialization
+	void Start () {
+        rgbd = GetComponent<Rigidbody2D>();
+        script = GetComponent<EnemyChase>();
+        rgbd.mass = mass;
+        script.speed = speed;
+        script.knockbackTimer = knockbackTimer;
+	}
+
+    void damage(float[] results)
+    {
+        health -= results[0];
+        script.SendMessage("knockback", results[1]);
+        Debug.Log(health);
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
