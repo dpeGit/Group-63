226d245d0bc69d0708e04ae24f99bf48aa16d581
Sam Tate <sjt75@drexel.edu>
2018-03-14 16:18:32 -0400

    ToastMenus
    contains latest version of my ui and game in general

---
+++ TOASTMenus/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+    public void LoadCampfire()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+	public void LoadTemple()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
