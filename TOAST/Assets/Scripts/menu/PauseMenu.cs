8a8153db9a37e1b86947ce1d1d0ce316bdb92a63
DemonPopeEnterprises <mattmrad62@gmail.com>
2018-03-14 23:52:58 -0400

    Okay project is back its a start

---
+++ TOAST/Assets/Scripts/menu/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject Panel;
+    public bool paused;
+    public Button ResumeButton, Temple;
+
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (paused)
+            {
+                Panel.gameObject.SetActive(false);
+                paused = false;
+                Time.timeScale = 1f;
+                Temple.Select();
+            }
+            else
+            {
+                Panel.gameObject.SetActive(true);
+                paused = true;
+                Time.timeScale = 0f;
+                ResumeButton.Select();
+            }
+        }
+        
+	}
+
+    public void Resume()
+    {
+        Panel.gameObject.SetActive(false);
+        paused = false;
+        //Temple.Select();
+        Time.timeScale = 1f;
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ TOAST/Assets/Scripts/player/SwtichAttacks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwtichAttacks : MonoBehaviour {
+
+    private GameObject weapon1;
+    private GameObject weapon2;
+
+    private void Start()
+    {
+        weapon1 = GameObject.Find("axe");
+        weapon2 = GameObject.Find("dagger");
+        weapon1.SetActive(true);
+        weapon2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            weapon1.SetActive(true);
+            weapon2.SetActive(false);        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            weapon1.SetActive(false);
+            weapon2.SetActive(true);
+        }
+	}
+}
