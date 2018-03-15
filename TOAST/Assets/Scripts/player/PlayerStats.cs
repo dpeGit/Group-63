5a28e9da88dfe2af19bed9aca478156186e9d09d
Sam Tate <sjt75@drexel.edu>
2018-03-15 12:34:44 -0400

    PlayerStats
    added playerstats script

---
+++ TOAST/Assets/Scripts/player/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+    public int exp, expNeeded;
+    public int level;
+
+	// Use this for initialization
+	void Start () {
+        exp = 0;
+        expNeeded = 1000;
+        level = 1;
+	}
+        
+
+	void Update () {
+        //leveling
+        if (exp >= expNeeded)
+        {
+            level++;
+            exp = exp % expNeeded;
+            expNeeded += 1000;
+        }
+    }
+}
