2a51567f9cac190e0268982640507f4ae4335307
Sam Tate <sjt75@drexel.edu>
2018-04-12 14:48:17 -0400

    Inventory
    Initial inventory D

---
+++ TOAST/Assets/Scripts/UI/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Inventory : MonoBehaviour {
+
+    GameObject[] weapons = new GameObject[15];
+    GameObject[] Armor = new GameObject[15];
+
+    void onPickup(GameObject item)
+    {
+        if (item.tag == "weapon")
+            Array.find[]
+    }
+
+
+}
