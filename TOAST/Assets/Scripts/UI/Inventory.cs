686c271b66e6ee0896f0d6dfbc072fffe0e5e3f8
Sam Tate <sjt75@drexel.edu>
2018-04-16 16:43:12 -0400

    Inventory
    updated inventory script

--- TOAST/Assets/Scripts/UI/Inventory.cs
+++ TOAST/Assets/Scripts/UI/Inventory.cs
@@ -9,11 +9,51 @@ public class Inventory : MonoBehaviour {
     GameObject[] weapons = new GameObject[15];
     GameObject[] Armor = new GameObject[15];
 
-    void onPickup(GameObject item)
+    public GameObject empty;
+
+    private void Start()
     {
-        if (item.tag == "weapon")
-            Array.find[]
+        for (int i = 0; i < 15; i++)
+        {
+            weapons[i] = empty;
+            Armor[i] = empty;
+
+        }
     }
 
+    void updateInv()
+    {
+
+    }
+
+    private int findFirstEmpty(GameObject[] inv)
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            if (inv[i].CompareTag("empty"))
+                return i;
+        }
+        return -1;
+    }
+
+    void onPickup(GameObject item)
+    {
+        int loc = 0;
+
+        if (item.tag == "weapon")
+        {
+            loc = findFirstEmpty(weapons);
+            if (loc >= 0)
+                weapons[loc] = item;
+        }
+
+        if (item.tag == "armor")
+        {
+            loc = findFirstEmpty(Armor);
+            if (loc >= 0)
+                Armor[loc] = item;
+        }
+
+    }
 
 }
