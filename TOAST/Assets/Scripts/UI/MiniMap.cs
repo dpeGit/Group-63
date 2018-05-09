4ec950708a64c033affc82dcc10080eca8c31ed1
Sam Tate <sjt75@drexel.edu>
2018-05-09 18:52:53 -0400

    Minimap
    Minimap added and HUD reworked

---
+++ TOAST/Assets/Scripts/UI/MiniMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMap : MonoBehaviour {
+
+	public GameObject mapArea;
+	public GameObject rooms, start, end, blank;
+
+	GridLayoutGroup grid;
+	private int[,] map = ProceduralGeneration.mapLayout;
+	int rows;
+	int cols;
+
+	void Start (){
+		rows = map.GetLength (0);
+		cols = map.GetLength (1);
+		drawMinimap ();
+		grid = mapArea.GetComponent<GridLayoutGroup> ();
+
+		grid.cellSize = new Vector2 (150 / rows, 150 / cols);
+	}
+
+	void drawMinimap(){
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				if (map[r,c] == 3)
+					Instantiate (start, mapArea.transform);
+				else if (map[r,c] == 4)
+					Instantiate (end, mapArea.transform);
+				else if (map[r,c] > 0)
+					Instantiate (rooms, mapArea.transform);
+				else
+					Instantiate (blank, mapArea.transform);
+			}
+		}
+
+
+	}
+	
+
+}
--- TOAST/Assets/Scripts/UI/PlayerHUD.cs
+++ TOAST/Assets/Scripts/UI/PlayerHUD.cs
@@ -8,8 +8,7 @@ public class PlayerHUD : MonoBehaviour {
 
     public GameObject player;
     public Slider expSlider, hpSlider, manaSlider;
-    public Text level, exp, expNeeded;
-    public Text hp, maxhp, mana, maxmana;
+	public Text level;
 
 	
     void Start()
@@ -21,15 +20,7 @@ public class PlayerHUD : MonoBehaviour {
 	void Update () {
         expSlider.value = (playerStats.exp / (float)playerStats.expNeeded) * 100;
         level.text = playerStats.level.ToString();
-        exp.text = playerStats.exp.ToString();
-        expNeeded.text = playerStats.expNeeded.ToString();
-
         hpSlider.value = (playerStats.currentHealth / (float)playerStats.maxHealth) * 100;
-        hp.text = playerStats.currentHealth.ToString();
-        maxhp.text = playerStats.maxHealth.ToString();
-
 		manaSlider.value = (playerStats.currentMana / (float)playerStats.maxMana) * 100;
-		mana.text = playerStats.currentMana.ToString();
-		maxmana.text = playerStats.maxMana.ToString();
     }
 }
