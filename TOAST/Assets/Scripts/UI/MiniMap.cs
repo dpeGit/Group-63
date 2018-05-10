9323eac05643c6bb926fad2ddf1af4910b06f840
Sam Tate <sjt75@drexel.edu>
2018-05-09 21:36:37 -0400

    Minimap Update
    gold room is your current location

--- TOAST/Assets/Scripts/UI/MiniMap.cs
+++ TOAST/Assets/Scripts/UI/MiniMap.cs
@@ -6,26 +6,29 @@ using UnityEngine.UI;
 public class MiniMap : MonoBehaviour {
 
 	public GameObject mapArea;
-	public GameObject rooms, start, end, blank;
+	public GameObject rooms, start, end, blank, currentRoom;
 
 	GridLayoutGroup grid;
 	private int[,] map = ProceduralGeneration.mapLayout;
+	private int[] location;
 	int rows;
 	int cols;
 
 	void Start (){
 		rows = map.GetLength (0);
 		cols = map.GetLength (1);
-		drawMinimap ();
+		location = Doors.location;
 		grid = mapArea.GetComponent<GridLayoutGroup> ();
-
 		grid.cellSize = new Vector2 (150 / rows, 150 / cols);
+		drawMinimap ();
 	}
 
 	void drawMinimap(){
 		for (int r = 0; r < rows; r++) {
 			for (int c = 0; c < cols; c++) {
-				if (map[r,c] == 3)
+				if (r == location [0] && c == location [1])
+					Instantiate (currentRoom, mapArea.transform);
+				else if (map[r,c] == 3)
 					Instantiate (start, mapArea.transform);
 				else if (map[r,c] == 4)
 					Instantiate (end, mapArea.transform);
